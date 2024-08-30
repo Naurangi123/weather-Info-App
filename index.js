3ad1b4dff3300144e08f3479a38058c4d6f5568c
Naurangi_lal <naurangilal15072002@gmail.com>
2024-08-31 00:22:22 +0530

    Add gitignore and delete cname

--- index.js
+++ index.js
@@ -7,41 +7,35 @@ require('dotenv').config();
 
 var bodyParser = require("body-parser");
 
-//in nodejs we have builtin middleware that does everthings for us
+
 app.use("/public", express.static("public"));
 
-//it tells express to use ejs as a viewengine that will then render dynamic content
 app.set("view engine", "ejs");
 
-// app.get("/", (req, res) => {
-//   console.log("Request made on:" + req.url);
+app.get("/", (req, res) => {
+  console.log("Request made on:" + req.url);
 
-//   requests(
-//     "http://api.openweathermap.org/data/2.5/weather?q=duhai&appid=344b134b52cf63f9d28bb5e7ea69259b&units=metric"
-//   )
-//     //if we get data then it goes through chunk and print data in console
-//     .on("data", function (chunk) {
-//       const objdata = JSON.parse(chunk); //converting json to object to use data in site
-//       //after this we can pass object in ejs site
+  requests(
+    "http://api.openweathermap.org/data/2.5/weather?q=duhai&appid=344b134b52cf63f9d28bb5e7ea69259b&units=metric"
+  )
+    .on("data", function (chunk) {
+      const objdata = JSON.parse(chunk); 
 
-//       const time = new Date().toLocaleTimeString();
+      const time = new Date().toLocaleTimeString();
 
-//       //we can pass data dynamic througn 2nd param as objects
-//       res.render("weather", { apidata: objdata, time: time });
-//       console.log(objdata);
-//     })
-//     //else through error
-//     .on("end", function (err) {
-//       if (err) return console.log("connection closed due to errors", err);
-//       console.log("end");
-//     });
-// });
+      res.render("weather", { apidata: objdata, time: time });
+      console.log(objdata);
+    })
+    //else through error
+    .on("end", function (err) {
+      if (err) return console.log("connection closed due to errors", err);
+      console.log("end");
+    });
+});
 
-// create application/x-www-form-urlencoded parser i.e middleware
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-//when user submit city to find its weather below things will happens
-//if user clicked submit it will submit all data to req.body object and that can be further stored in server
+
 app.post("/", urlencodedParser, function (req, res) {
   var reqCity = req.body.city; //data that was submitted by user
   requests(
@@ -65,7 +59,7 @@ app.post("/", urlencodedParser, function (req, res) {
       console.log("end");
     });
 });
- const PORT=5000;
+ const PORT=process.env.PORT||5000;
 //this express app server listen on port 5000
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
